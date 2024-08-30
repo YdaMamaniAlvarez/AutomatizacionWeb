99e99915f6cba82fcfda2ae3a7f1dd7a2e0de4b0
Yda Manuela <tiamydesign@gmail.com>
2024-08-30 03:26:54 -0500

    Se agregaron localizadores

--- src/test/java/com/nttdata/page/bensgStorePage.java
+++ src/test/java/com/nttdata/page/bensgStorePage.java
@@ -1,37 +1,32 @@
 package com.nttdata.page;
 
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class bensgStorePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
 
     //Localizadores de elementos
-    public static By emailField = By.id("field-email");
-    public static By passwordField = By.id("field-password");
-    public static By loginButton = By.cssSelector("submit-login");
-    //public static By userInput = By.id("field-email");
-    //public static By passInput = By.id("field-password");
-    //public static By loginButton = By.id("submit-login");
+    public static final By userInput = By.id("field-email");
+    public static final By passInput = By.id("field-password");
+    public static final By loginButton = By.id("submit-login");
+    /*de aqui para arriba no tocar nada */
 
-    // localizador para el elemento de categoría
-    public static By category3 = By.id("category-3");
+    // localizador para el elemento de categoría Clothes
+    public static final By getCategory3 = By.xpath("//a[@href='https://qalab.bensg.com/store/es/3-clothes']");
+    // localizador para el elemento de sub-categoría Men
+    public static final By getCategory4 = By.xpath("//a[@href='https://qalab.bensg.com/store/es/4-men']");
 
-    // localizador para la categoría "Men"
-    public static By categoryMen = By.xpath("//li[@class='category']//a[@class='dropdown-item dropdown-submenu' and contains(text(), 'Men')]");
 
-    // localizador para el botón "Añadir al carrito" botón agrego 2 unidades del primer producto al carrito
-    public static By addToCartButton = By.xpath("//button[@class='btn btn-primary add-to-cart' and @data-button-action='add-to-cart' and contains(., 'Añadir al carrito')]");
 
-    // localizador usando XPath con atributo personalizado
-    //popup la confirmación del producto agregado
-    public static By modalContent = By.xpath("//div[@class='modal-content' and @data-testid='unique-modal-content']");
 
-    // localizador para el span que contiene el monto
-    public static By priceValue = By.xpath("//span[@class='value' and contains(text(), 'PEN')]");
-
-    //valido el titulo de la pagina del carrito
-    public static By logoImage = By.cssSelector("div#_desktop_logo img");
-
-    /*public static By productsTitle = By.cssSelector("span.title");
-    public static By itemsCards = By.cssSelector("div.inventory_item");*/
 }
