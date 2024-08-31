7d7baba250356bab47a166fa452d4adfc5d644fa
Yda Manuela <tiamydesign@gmail.com>
2024-08-31 05:37:43 -0500

    Update bensgStorePage.java
    Se agregaron nuevos localizadores para selección de cantidad de producto

--- src/test/java/com/nttdata/page/bensgStorePage.java
+++ src/test/java/com/nttdata/page/bensgStorePage.java
@@ -19,12 +19,20 @@ public class bensgStorePage {
     public static final By userInput = By.id("field-email");
     public static final By passInput = By.id("field-password");
     public static final By loginButton = By.id("submit-login");
-    /*de aqui para arriba no tocar nada */
 
     // localizador para el elemento de categoría Clothes
     public static final By getCategory3 = By.xpath("//a[@href='https://qalab.bensg.com/store/es/3-clothes']");
     // localizador para el elemento de sub-categoría Men
     public static final By getCategory4 = By.xpath("//a[@href='https://qalab.bensg.com/store/es/4-men']");
+    /*de aqui para arriba no tocar nada */
+
+    //Localizador para elemento Producto en sección Men
+    public static final By primerProductoMen = By.xpath("//a[@href='https://qalab.bensg.com/store/es/men/1-1-hummingbird-printed-t-shirt.html#/1-tamano-s/8-color-blanco']");
+
+    //Localizador para el elemento botón spin Up para aumentar cantidad de productos
+    public static final By btnSpinUpProducto = By.xpath("//button/i[contains(@class, 'touchspin-up')]");
+
+
 
 
 
